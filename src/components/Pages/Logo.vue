5388923213f92508dfa6cb792959018209850000
iseyoshitaka <ise0615@gmail.com>
2022-11-03 10:35:50 +0900

    pugを導入

--- src/components/Pages/Logo.vue
+++ src/components/Pages/Logo.vue
@@ -1,9 +1,7 @@
-<template>
-  <NuxtLink :to="Url.TOP">
-    <v-toolbar-title
-      ><h1 class="md:text-2xl">isystk's Portfolio</h1></v-toolbar-title
-    >
-  </NuxtLink>
+<template lang="pug">
+NuxtLink(:to='Url.TOP')
+  v-toolbar-title
+    h1(class="md:text-2xl") isystk&apos;s Portfolio
 </template>
 
 <script setup lang="ts">
